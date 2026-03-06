983453f93f13a6e21c2751672761e0a8e9b84859
Bhanu8213 <estharibhanuprasad@gmail.com>
2026-03-06 17:12:24 +0530

    Added FarmerApp models

--- backend/FarmerMarketing/FarmerApp/models.py
+++ backend/FarmerMarketing/FarmerApp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Farmer(models.Model):
+    name=models.CharField(max_length=100)
+    phone=models.CharField(max_length=15)
+    location=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name=models.CharField(max_length=100)
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+    Farmer = models.ForeignKey(Farmer,
+    on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    product = models.ForeignKey(Product,
+on_delete=models.CASCADE)
+    customer_name =models.CharField(max_length=100)
+    quantity=models.IntegerField()
